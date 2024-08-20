c2a4956fcc2290fd358a5d8fa34a8bf8385f0494
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-08-20 21:19:52 +0200

    responsiveness

--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -6,6 +6,7 @@ import { SCORE_VALUE_ID } from '../constants.js';
 export const createScoreElement = (score) => {
   const element = document.createElement('div');
   element.classList.add('score');
+  element.classList.add('score-container');
   element.innerHTML = String.raw`
   Your score: <span id="${SCORE_VALUE_ID}">${score}</span> / 10
   `;
