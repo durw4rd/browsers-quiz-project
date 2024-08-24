654aa50ab3e443ee537039866f89b6d64d0ef2b5
Alexander Vasyliev <saddon11@gmail.com>
2024-08-24 11:38:26 +0200

    fix: refresh-page-on-first-question

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@ import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 import { initQuestionPage } from './pages/questionPage.js';
 const loadApp = () => {
-  const currentQuestionStored = Number(localStorage.getItem('currentQuestion'));
-
+  const currentQuestionStored = localStorage.getItem('currentQuestion');
   if (currentQuestionStored) {
-    quizData.currentQuestionIndex = currentQuestionStored;
+    quizData.currentQuestionIndex = Number(currentQuestionStored);
     initQuestionPage();
   } else {
     quizData.currentQuestionIndex = 0;
