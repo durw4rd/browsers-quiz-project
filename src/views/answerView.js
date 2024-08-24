90c8795ca4797dbb9f767e318783dd9cf49deba4
Sofiia <101416352+SoniaKol@users.noreply.github.com>
2024-08-24 16:46:41 +0200

    done: minor changes
    - remove semicolons from answers
    - add question number
    - add users name and avatar on every questions page
    - add button style to results page

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -6,7 +6,7 @@ export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
   element.dataset.key = key;
   element.innerHTML = String.raw`
-    ${answerText};
+    ${answerText}
   `;
   return element;
 };
--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -4,11 +4,13 @@ import { SCORE_VALUE_ID } from '../constants.js';
  * @returns {Element}
  */
 export const createScoreElement = (score) => {
+  const user = JSON.parse(localStorage.getItem('user'));
   const element = document.createElement('div');
   element.classList.add('score');
   element.classList.add('score-container');
   element.innerHTML = String.raw`
-  Your score: <span id="${SCORE_VALUE_ID}">${score}</span> / 10
+    <img src="./public/${user.playerAvatar}" alt="User avatar" class="question-avatar-image">
+    <p>${user.playerName}, your score: <span id="${SCORE_VALUE_ID}">${score}</span> / 10</p>
   `;
   return element;
 };
